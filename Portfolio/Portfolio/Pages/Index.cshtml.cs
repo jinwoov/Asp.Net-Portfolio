20c5a3af84587cd47941886b8d35508f8189bf2f
Jin Kim <jinwoov@gmail.com>
2020-04-26 23:48:25 -0700

    90% done

--- Portfolio/Portfolio/Pages/Index.cshtml.cs
+++ Portfolio/Portfolio/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,30 @@ namespace Portfolio.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public EmailInfo EmailInfo { get; set; }
         public void OnGet()
         {
         }
+
+        public void OnPost()
+        {
+            Console.WriteLine(EmailInfo);
+        }
+    }
+
+    public class EmailInfo
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
     }
 }
